5b6eb6db2058d08a4320f34214196981cff041c2
Alkelchamaa <codebraver52@codebrave.org>
2024-12-16 16:42:28 +0200

    portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var loader= document.getElementById("preloader")
+window.addEventListener("load", function(){loader.style.display="none";})
+document.getElementById("demo").onmouseover = function() {mouseOver()};
+document.getElementById("demo").onmouseout = function() {mouseOut()};
+
+
+
